b57afb3a53240fed623acd63f3bad0e63c7fa63c
Jeewan Ghimire <jeewanghimire31@gmail.com>
2023-02-04 21:16:15 +0545

    initial commit

---
+++ lib/constants/questions.dart
@@ -0,0 +1,9 @@
+var questions = [
+  {"question": "1+3", "answer": 4},
+  {"question": "3+2", "answer": 5},
+  {"question": "120+140", "answer": 260},
+  {"question": "50+30", "answer": 80},
+  {"question": "60+30", "answer": 90},
+  {"question": "70+30", "answer": 100},
+  {"question": "80+30", "answer": 110},
+];
